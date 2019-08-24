e88160d2de2882708a8efc8d395d6688889d87be
fnvn <mshitcake@gmail.com>
2019-08-24 13:31:39 -0300

    whit hello world

--- newEmptyPHPWebPage.php
+++ newEmptyPHPWebPage.php
@@ -10,7 +10,7 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <h1> Hello people</h1>
+        <h1> Hello world</h1>
         <?php
         // put your code here
         ?>
